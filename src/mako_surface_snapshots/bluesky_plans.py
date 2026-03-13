52b0cb9eae93e686c99fba16f126690ebcc7c9c2
Anja F. Hörmann <anja.hoermann@bam.de>
2026-03-13 13:06:27 +0100

    configure camera exposure time and save image (manually)

--- src/mako_surface_snapshots/bluesky_plans.py
+++ src/mako_surface_snapshots/bluesky_plans.py
@@ -1,17 +1,36 @@
 from __future__ import annotations
 from typing import Iterator
+from pathlib import Path
 
 from bluesky import plan_stubs as bps
 
+from mako_surface_snapshots.devices import ad_configure_exposure
 
-def vimba_capture(vimba, exposure_time: float = 1) -> Iterator:
+
+def vimba_capture(
+    vimba, exposure_time: float = 1, output_path: Path = Path("/tmp/current/")
+) -> Iterator:
+    yield from ad_configure_exposure(
+        vimba, exposure_time=exposure_time, output_path=output_path
+    )
     yield from bps.stage(vimba)
     try:
         yield from bps.trigger_and_read([vimba], name="mouse_vimba_measure")
     finally:
         yield from bps.unstage(vimba)
 
-def vimba_read(vimba, exposure_time: float = 1) -> Iterator:
+
+def vimba_read(
+    vimba, exposure_time: float = 1, output_path: Path = Path("/tmp/current/")
+) -> Iterator:
     yield from bps.open_run()
-    yield from vimba_capture(vimba, exposure_time = 1)
+    yield from vimba_capture(
+        vimba, exposure_time=exposure_time, output_path=output_path
+    )
+    array_size = vimba.image.array_size.get()
+    shape = (array_size.height, array_size.width)
+    img = vimba.image.array_data.get().reshape(shape)
+    plt.imshow(img)
+    plt.savefig(output_path / "latest.png")
+    plt.close()
     yield from bps.close_run()
--- src/mako_surface_snapshots/devices.py
+++ src/mako_surface_snapshots/devices.py
@@ -30,3 +30,31 @@ class Vimba(SingleTrigger, DetectorBase):
     cam = ADComponent(CamBase, "cam1:")
     image = ADComponent(ImagePlugin, "image1:")
     stats1 = ADComponent(StatsPlugin, "Stats1:")
+    output_path = Path("/tmp/images/")
+
+
+def ad_configure_exposure(
+    det, exposure_time: int = 1, output_path: Path | str = "/tmp/current/"
+):
+    def split_exposure_time_to_frames_and_time(exposure_time):
+        # Eiger can only acquire frames of up to 10s, so we need to split the exposure time into multiple frames if it's longer than that.
+        if exposure_time <= 0:
+            exposure_time = 1  # dumbasses.
+        if exposure_time <= 10:
+            return 1, exposure_time
+        else:
+            n_frames = int(np.ceil(exposure_time / 10))
+            frame_time = exposure_time / n_frames
+            return n_frames, frame_time
+
+    n_frames, frame_time = split_exposure_time_to_frames_and_time(exposure_time)
+    out_path = Path(output_path)  # this is a directory
+    if not out_path.exists():
+        # create
+        out_path.mkdir(parents=True, exist_ok=True)
+    out_path = out_path.as_posix()  # convert to string for EPICS
+
+    yield from bps.mv(det.cam.num_images, int(n_frames))
+    yield from bps.mv(det.cam.acquire_time, frame_time)
+    yield from bps.mv(det.cam.acquire_period, frame_time)
+    det.output_path = Path(output_path)
