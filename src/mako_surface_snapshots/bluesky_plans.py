2807d3922f70cc473cbef33e44a2248e14ddc415
Anja F. Hörmann <anja.hoermann@bam.de>
2026-03-13 11:37:33 +0100

    fix: replace eiger by vimba

--- src/mako_surface_snapshots/bluesky_plans.py
+++ src/mako_surface_snapshots/bluesky_plans.py
@@ -7,6 +7,6 @@ from bluesky import plan_stubs as bps
 def vimba_capture(vimba, exposure_time: float = 1) -> Iterator:
     yield from bps.stage(vimba)
     try:
-        yield from bps.trigger_and_read([eiger], name="mouse_eiger_measure")
+        yield from bps.trigger_and_read([vimba], name="mouse_vimba_measure")
     finally:
-        yield from bps.unstage(eiger)
+        yield from bps.unstage(vimba)
