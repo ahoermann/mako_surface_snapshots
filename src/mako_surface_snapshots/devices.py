7983a3288de7519fc6d7f3b37f2c6852b805fbb3
Anja F. Hörmann <anja.hoermann@bam.de>
2026-03-13 13:45:50 +0100

    comment out modification of paths during configuration

--- src/mako_surface_snapshots/devices.py
+++ src/mako_surface_snapshots/devices.py
@@ -89,7 +89,8 @@ def ad_configure_exposure(
     yield from bps.mv(det.cam.acquire_time, frame_time)
     yield from bps.mv(det.cam.acquire_period, frame_time)
     yield from bps.mv(det.hdf1.num_capture, 1)
-    yield from bps.mv(det.hdf1.write_path_template, output_path / "%Y/%m/%d/")
+    # not sure what the correct way to configure paths is here
+    # yield from bps.mv(det.hdf1.write_path_template, output_path / "%Y/%m/%d/")
 
     det.read_attrs.append("hdf1")
     enabled = det.hdf1.enable.get()
